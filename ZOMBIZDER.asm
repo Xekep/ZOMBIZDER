0e4b95b237853ad002f93efa7e0cf104254f091b
Xekep <kidlok@bk.ru>
2015-08-31 09:35:11 +0500

    fix

--- ZOMBIZDER.asm
+++ ZOMBIZDER.asm
@@ -7,27 +7,33 @@ sinfo STARTUPINFO
 lpflOldProtec dd ?
 ZombiPath db '%s/steamapps/common/ZOMBI/',0
 ZombiExe db 'ZOMBI.EXE',0
+SteamOverlayExe db 'GameOverlayUI.exe',0
 RegSubKey db 'Software\Valve\Steam',0
 RegKey db 'SteamPath',0
 byte1 db 0ebh
 byte2 db 0c3h,90h
+byte3 db 88h
 SteamPath rb 512
 StartPath rb 512
 HKey dd ?
 sz dd 512
+steam db 0
 
 .code
 start:
 	invoke RegOpenKeyEx,HKEY_CURRENT_USER,RegSubKey,0,KEY_QUERY_VALUE,HKey
 	.if eax=0
 		invoke RegQueryValueExA,[HKey],RegKey,0,0,SteamPath,sz
+		.if eax=0
+			mov [steam],1
+			cinvoke wsprintfA,StartPath,ZombiPath,SteamPath
+			invoke SetCurrentDirectoryA,StartPath
+		.endif
 		invoke RegCloseKey,[HKey]
 	.else
 		invoke MessageBoxA,0,'Стимопуть не найден.','Ирар',MB_ICONERROR+MB_OK
 		jmp .exit
 	.endif
-	cinvoke wsprintfA,StartPath,ZombiPath,SteamPath
-	invoke SetCurrentDirectoryA,StartPath
 	invoke lstrcatA,StartPath,ZombiExe
 	invoke CreateProcessA,StartPath,0,0,0,0,CREATE_SUSPENDED,0,0,sinfo,pinfo
 	.if eax<>0
@@ -50,6 +56,13 @@ start:
 			invoke WriteProcessMemory,[pinfo.hProcess],009B396Ah,byte2,2,lpflOldProtec
 			.if eax<>0
 				invoke FlushInstructionCache,[pinfo.hProcess],009B396Ah,2
+			.endif
+		.endif
+		invoke VirtualProtectEx,[pinfo.hProcess],005F3101h,1,PAGE_EXECUTE_READWRITE,lpflOldProtec
+		.if eax<>0
+			invoke WriteProcessMemory,[pinfo.hProcess],005F3101h,byte3,1,lpflOldProtec
+			.if eax<>0
+				invoke FlushInstructionCache,[pinfo.hProcess],005F3101h,1
 			.endif
 		.endif
 		invoke ResumeThread,[pinfo.hThread]
